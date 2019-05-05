abef5c3f9ec860bd9809856f996f479f013a8582
Michael Nease <michaelnease@michaelnease.com>
2019-05-05 15:51:07 -0700

    handleSendMessage

--- components/Contact.js
+++ components/Contact.js
@@ -1,12 +1,16 @@
 import { Form } from "./styles";
 
 const Contact = () => {
+  const handleSendMessage = () => {
+    console.log("send message");
+  };
+
   return (
     <Form>
       <input type="text" placeholder="Name" />
       <input type="email" placeholder="Email" />
       <textarea name="message" rows="10" cols="30" placeholder="Message" />
-      <button>Send Message</button>
+      <button onClick={handleSendMessage}>Send Message</button>
     </Form>
   );
 };

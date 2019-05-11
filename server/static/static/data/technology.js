fb317efb96c6daf3e7e9103e25ec2b6fc3e1acad
Michael Nease <michaelnease@michaelnease.com>
2019-05-11 13:41:49 -0700

    server run dist

---
+++ server/static/static/data/technology.js
@@ -0,0 +1,62 @@
+export const technology = [
+  {
+    id: 1,
+    name: "React",
+    rating: 8.5
+  },
+  {
+    id: 2,
+    name: "Redux",
+    rating: 8.5
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    rating: 9
+  },
+  {
+    id: 4,
+    name: "CSS",
+    rating: 9
+  },
+  {
+    id: 5,
+    name: "HTML",
+    rating: 9
+  },
+  {
+    id: 6,
+    name: "Node",
+    rating: 8
+  },
+  {
+    id: 7,
+    name: "Express",
+    rating: 9
+  },
+  {
+    id: 8,
+    name: "MongoDB",
+    rating: 8
+  },
+  {
+    id: 9,
+    name: "Docker",
+    rating: 8.5
+  },
+  {
+    id: 10,
+    name: "Git",
+    rating: 9
+  },
+  {
+    id: 11,
+    name: "Ruby",
+    rating: 8.5
+  },
+  {
+    id: 12,
+    name: "AWS",
+    rating: 8
+  }
+];

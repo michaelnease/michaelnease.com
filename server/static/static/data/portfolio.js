38cb0dc860e9af5d42a5b4e17a3b9cd2ddd47a0c
Michael Nease <michaelnease@michaelnease.com>
2019-05-11 13:48:10 -0700

    server build files with node

--- server/static/static/data/portfolio.js
+++
@@ -1,9 +0,0 @@
-export const portfolio = [
-  { id: 1, src: "drumoff2011.jpg", span: 2 },
-  { id: 2, src: "drumoff2010.jpg", span: 2 },
-  { id: 3, src: "sessions.jpg", span: 2 },
-  { id: 4, src: "platinum-home.jpg", span: 1 },
-  { id: 5, src: "platinum-pdp.jpg", span: 1 },
-  { id: 6, src: "onlyincalifornia.jpg", span: 1 },
-  { id: 7, src: "powerlevelreview.jpg", span: 1 }
-];

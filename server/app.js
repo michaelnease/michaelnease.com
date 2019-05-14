6d4bc51ae3ece32b515f1dab2786cc225e6f8ce5
Michael Nease <michaelnease@michaelnease.com>
2019-05-13 22:58:05 -0700

    server routes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var contactRouter = require("./routes/contact");
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "static")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/contact", contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

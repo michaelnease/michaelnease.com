c2a2141d6f785430e1b38aad9931bc4eae4b3c3c
Michael Nease <michaelnease@michaelnease.com>
2019-06-01 21:32:24 -0700

    google analytics

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,8 +1,17 @@
 import { Component } from "react";
 import { ThemeProvider } from "styled-components";
+import ReactGA from "react-ga";
 import Meta from "./Meta";
 import GlobalStyles from "./styles/Global-Styles";
 import { theme } from "./styles/theme";
+
+const DEFAULT_CONFIG = {
+  trackingId: "UA-5643379-1",
+  debug: false,
+  gaOptions: {
+    cookieDomain: "none"
+  }
+};
 
 export class Layout extends Component {
   render() {

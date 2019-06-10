c0fe36643d9f09f99ab4202c1ad71134e369e872
Michael Nease <michaelnease@michaelnease.com>
2019-06-09 21:00:11 -0700

    setup env vars

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var nodemailer = require("nodemailer");
-require("dotenv").config();
+// require("dotenv").config();
 
 router.post("/", function(req, res, next) {
   const { name, email, message } = req.body;
